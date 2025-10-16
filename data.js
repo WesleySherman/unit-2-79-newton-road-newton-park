e1af57c4c64a4b96dcf1fb41d364f9632d13f38e
Wesley Sherman <your-email@domain.com>
2025-10-16 17:19:48 +0200

    Fresh upload of 360 walkthrough (Newton Road Unit 2)

---
+++ data.js
@@ -0,0 +1,335 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---outside-of-unit",
+      "name": "01 - Outside of Unit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.17033348790792857,
+        "pitch": 0.04347216682305444,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1655493248517086,
+          "pitch": 0.30501733228535244,
+          "rotation": 0,
+          "target": "1-02---reception-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---reception-area",
+      "name": "02 - Reception Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.4787590894163962,
+        "pitch": 0.005272038778684873,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9571715808649834,
+          "pitch": 0.41324861781906463,
+          "rotation": 0,
+          "target": "0-01---outside-of-unit"
+        },
+        {
+          "yaw": 1.3021260267716173,
+          "pitch": 0.4793043300758786,
+          "rotation": 1.5707963267948966,
+          "target": "2-03---office-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---office-01",
+      "name": "03 - Office 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5585439946454223,
+        "pitch": -0.06179892344323079,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.539161961631901,
+          "pitch": 0.4479102358258551,
+          "rotation": 1.5707963267948966,
+          "target": "3-04---office-02"
+        },
+        {
+          "yaw": -2.721077135300483,
+          "pitch": 0.42498547168912637,
+          "rotation": 4.71238898038469,
+          "target": "1-02---reception-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---office-02",
+      "name": "04 - Office 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.019991953250112715,
+        "pitch": -0.041861702866423656,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.726885068031187,
+          "pitch": 0.43344465463061255,
+          "rotation": 1.5707963267948966,
+          "target": "4-05---office-03"
+        },
+        {
+          "yaw": -2.9106602010880973,
+          "pitch": 0.4581223148140712,
+          "rotation": 4.71238898038469,
+          "target": "2-03---office-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---office-03",
+      "name": "05 - Office 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.4936845071158835,
+        "pitch": 0.05885085185195926,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9486956166106957,
+          "pitch": 0.4323832498224647,
+          "rotation": 1.5707963267948966,
+          "target": "5-06---open-plan-office"
+        },
+        {
+          "yaw": 2.782042041678075,
+          "pitch": 0.4056082981328206,
+          "rotation": 4.71238898038469,
+          "target": "3-04---office-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---open-plan-office",
+      "name": "06 - Open Plan Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.42483439453092764,
+        "pitch": 0.02851410624121975,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7855519463612701,
+          "pitch": 0.3810425582402832,
+          "rotation": 0,
+          "target": "6-07---kitchenette-and-bathroom"
+        },
+        {
+          "yaw": 2.5879572903827723,
+          "pitch": 0.38561636446913994,
+          "rotation": 0,
+          "target": "4-05---office-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---kitchenette-and-bathroom",
+      "name": "07 - Kitchenette and Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.15421876367905263,
+        "pitch": 0.005232712858301625,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1545374933468153,
+          "pitch": 0.39384704699719464,
+          "rotation": 0,
+          "target": "5-06---open-plan-office"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Unit 2, 79 Newton Road, Newton Park",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
